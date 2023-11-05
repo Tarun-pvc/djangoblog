7926aaada13e3b46d370cef055432a5026c0aeab
Tarun <tarun.cosmic@gmail.com>
2023-11-05 07:11:47 +0530

    datecommit

--- blog/users/admin.py
+++ blog/users/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here. Thsi is another test
 
 
-# this is also for test, pls understand.
+# this is also for test, pls understand. man this is going to be the lst test
